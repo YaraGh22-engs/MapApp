6eee25d016bca3a723195bb5fa19f7f76c179bc7
Yara M Esmaiel <yaraesmaiel123@gmail.com>
2024-07-01 01:54:11 +0300

    Bing Map using

---
+++ GoogleMapApp/Controllers/CordController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoogleMapApp.Controllers
+{
+    public class CordController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
